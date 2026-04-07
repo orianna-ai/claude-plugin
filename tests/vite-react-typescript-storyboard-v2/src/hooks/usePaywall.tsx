30c2dc5d05060e0b451bda7a54b71231de95bca9
Ashwin Madavan <ashwin.madavan@gmail.com>
2026-04-07 09:18:24 -0400

    chore: move tests to a separate repository to make the clone smaller (#167)

--- tests/vite-react-typescript-storyboard-v2/src/hooks/usePaywall.tsx
+++
@@ -1,9 +0,0 @@
-export function usePaywall(_options: { successUrl: string; cancelUrl: string }) {
-  return {
-    showPaywall: false,
-    paywallData: null,
-    checkPaywall: async () => false,
-    handleUpgrade: () => {},
-    closePaywall: () => {},
-  };
-}
