a34fa6f7c083a2e616628a1a3ba0861547c75c24
Ashwin Madavan <ashwin.madavan@gmail.com>
2026-05-04 14:01:13 +0000

    linter

--- plugins/softlight/skills/softlight/run_agent.py
+++ plugins/softlight/skills/softlight/run_agent.py
@@ -120,8 +120,7 @@ def _steer_conversation(
             [
                 proposed_dicussion
                 for proposed_dicussion in project.get("proposed_discussions") or []
-                if discussion is None
-                or _created_at(proposed_dicussion) > _created_at(discussion)
+                if discussion is None or _created_at(proposed_dicussion) > _created_at(discussion)
             ],
         ]
 
