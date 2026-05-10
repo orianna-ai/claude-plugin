742fa684ea32e6b9fa4aefcd8c7139bcdf25fa6d
Ashwin Madavan <ashwin.madavan@gmail.com>
2026-05-10 14:10:28 +0000

    fix

--- plugins/softlight/skills/softlight/scripts/create_app.py
+++ plugins/softlight/skills/softlight/scripts/create_app.py
@@ -103,7 +103,7 @@ def _create_main_tsx(
 ) -> None:
     main_tsx = """\
 import { createRoot } from 'react-dom/client'
-import { App } from './App'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(<App />)
 """
